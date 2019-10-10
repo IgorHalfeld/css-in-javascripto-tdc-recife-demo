0c30d3785f00fa3eb4e3cab657f7ba6d9ca79ce9
Igor Halfeld <hello@igorluiz.me>
2019-10-09 23:17:33 -0300

    remove class style and add createElement method

--- styled-components/app.js
+++ styled-components/app.js
@@ -1,54 +1,81 @@
-function getRandomString() {
+const getRandomString = () => {
   const x = 2147483648;
   return (
     Math.floor(Math.random() * x).toString(36) +
     Math.abs(Math.floor(Math.random() * x) ^ Date.now()).toString(36)
   );
-}
+};
 
-class StyleSheet {
-  constructor() {
-    this.sheet = null;
-    this.init();
-  }
-
-  init() {
-    const styleEl = document.createElement('style');
-    document.head.appendChild(styleEl);
-    this.sheet = styleEl.sheet;
-  }
-  create(selector, styleString) {
-    console.log(styleString.toString())
-    const [style, hoverString] = styleString.toString().split('&');
-
-    const rule = `.${selector} { ${style} }`;
-    const ruleHover = hoverString && hoverString.length ? `.${selector}${hoverString}` : '';
-    const index = this.sheet.cssRules.length;
-
-    this.sheet.insertRule(rule, index);
-    this.sheet.insertRule(ruleHover, index);
-  }
-}
-
-
-class Styled {
-  constructor() {
-    this.styleSheet = new StyleSheet();
-  }
-
-  _createElement(el, className, attrs = {}) {
-    const element = document.createElement(el);
-    element.classList.add(className);
-    Object.keys(attrs).forEach(attr => {
-      element[attr] = attrs[attr];
-    });
+const setAllAttrsProperties = element => attrs => {
+  if (typeof attrs === 'string') {
+    element.textContent = attrs;
     return element;
   }
+  Object.keys(attrs).forEach(attr => element[attr] = attrs[attr]);
+  return element;
+};
 
-  div(styles) {
-    const className = `div-${getRandomString()}`;
-    this.styleSheet.create(className, styles);
-
-    return this._createElement('div', className);
+const setChildrens = (node, childrens) => {
+  if (!childrens) return node;
+  if (Array.isArray(childrens)) {
+    childrens.forEach(children => node.append(children));
+    return node;
   }
+  node.append(childrens);
+  return node;
 }
+
+const createElement = (el, attrs, childrens) => {
+  const element = document.createElement(el);
+  if (typeof attrs === 'string') {
+    element.textContent = attrs;
+    return element;
+  }
+  const putAttrs = setAllAttrsProperties(element);
+  return setChildrens(putAttrs(attrs), childrens);
+};
+
+const buildElement = (el, attrs, childrens) => {
+  const element = typeof el === 'string' ? document.querySelector(el) : el;
+  const node = setAllAttrsProperties(element)(attrs);
+  return setChildrens(node, childrens);
+};
+
+const useStyles = () => {
+  const styleEl = document.createElement('style');
+  document.head.appendChild(styleEl);
+  const sheet = styleEl.sheet;
+  return function create(selector, styleString) {
+    const [style, hoverString] = styleString.toString().split('&');
+
+    const rule = style && style.length ? `.${selector} { ${style} }` : null;
+    const ruleHover = hoverString && hoverString.length ? `.${selector}${hoverString}` : null;
+    const index = sheet.cssRules.length;
+
+    if (rule) sheet.insertRule(rule, index);
+    if (hoverString) sheet.insertRule(ruleHover, index);
+  };
+};
+
+const createStyledInstance = () => {
+  const createStyles = useStyles();
+  const DOM_ELEMENTS = ['div', 'p', 'h1', 'h2', 'h3', 'ul', 'li', 'hr', 'br', 'i'];
+
+  const createElementWithClass = (el, className, attrs = {}) => {
+    const element = document.createElement(el);
+    element.classList.add(className);
+    Object.keys(attrs).forEach(attr => element[attr] = attrs[attr]);
+    return element;
+  };
+
+  const build = (el, styles) => {
+    const className = `${el}-${getRandomString()}`;
+    createStyles(className, styles);
+    return createElementWithClass(el, className);
+  };
+
+  return DOM_ELEMENTS.reduce((acc, cur) => ({
+    ...acc,
+    [cur]: styles => build(cur, styles),
+  }), {});
+};
